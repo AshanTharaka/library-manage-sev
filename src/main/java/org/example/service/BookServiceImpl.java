5ee3ea44b54181d66431d95347400f0b450c306b
AshanTharaka <ashantharakaweerasekaraatw@gmail.com>
2025-01-18 23:28:31 +0530

    First commit

---
+++ src/main/java/org/example/service/BookServiceImpl.java
@@ -0,0 +1,10 @@
+package org.example.service;
+
+import org.example.dto.Book;
+
+public class BookServiceImpl implements BookService{
+    @Override
+    public void addBook(Book book) {
+
+    }
+}
