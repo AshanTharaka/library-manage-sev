7b0c5a18408089b905d840de51f9ecd0f200cb77
AshanTharaka <ashantharakaweerasekaraatw@gmail.com>
2025-01-19 20:33:15 +0530

    add swagger dep

--- src/main/java/org/example/service/impl/BookServiceImpl.java
+++ src/main/java/org/example/service/impl/BookServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -32,5 +33,21 @@ public class BookServiceImpl implements BookService {
     @Override
     public List<BookEntity> getBooks() {
         return repository.findAll();
+    }
+
+    @Override
+    public boolean deleteBook(Long id) {
+        if(repository.existsById(id)){
+            repository.deleteById(id);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public Book getBookById(Long id) {
+        Optional<BookEntity> byId=repository.findById(id);
+        return mapper.map(byId,Book.class);
     }
 }
